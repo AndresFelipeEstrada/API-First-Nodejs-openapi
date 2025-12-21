3d2fc1b06200322aaefa41646484c3aac2cb8182
Andres Felipe Estrada <andresfe1396@gmail.com>
2025-12-20 20:42:34 -0500

    add: express-openapi-validator

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import express from "express";
 import swaggerUI from "swagger-ui-express";
 import YAML from "yamljs";
+import OpenApiValidator from "express-openapi-validator";
 
 const app = express();
 const PORT = 3000;
@@ -9,7 +10,23 @@ const swaggerDocument = YAML.load("./openapi.yaml");
 
 app.use("/docs", swaggerUI.serve, swaggerUI.setup(swaggerDocument));
 
-app.get("/", (req, res) => {
+app.use(
+  OpenApiValidator.middleware({
+    apiSpec: swaggerDocument,
+    validateRequests: true,
+    validateResponses: true,
+    ignorePaths: /.*\/docs.*/,
+  }),
+);
+
+app.use((err, req, res, next) => {
+  res.status(err.status || 500).json({
+    message: err.message,
+    errors: err.errors,
+  });
+});
+
+app.get("/hello", (_req, res) => {
   res.json({ success: "hello" });
 });
 
